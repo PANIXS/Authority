1b4eb1b68cc26ce2ce2182a49b714411119fcfa0
Li Pan <1160728236@qq.com>
2018-01-05 17:10:17 +0800

    第八次提交

--- src/main/java/com/mmall/param/TestVo.java
+++ src/main/java/com/mmall/param/TestVo.java
@@ -7,13 +7,16 @@ import org.hibernate.validator.constraints.NotBlank;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Getter
 @Setter
 public class TestVo {
-    @NotBlank(message = "i cant be null")
+    @NotBlank(message = "msg:人家不能为空")
     private String msg;
     @NotNull(message = "id不可以为空")
     @Max(value = 10,message = "id不能大于10")
     @Min(value = 0,message = "id姚道玉扥估10")
     private Integer id;
+    private List<String> str;
 }
