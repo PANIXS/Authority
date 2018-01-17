2166010bad9834fd9c54792d8324f7dcfb03ed56
Li Pan <1160728236@qq.com>
2018-01-17 16:29:36 +0800

    第二十九次提交

--- src/main/java/com/mmall/service/SysCoreService.java
+++ src/main/java/com/mmall/service/SysCoreService.java
@@ -6,11 +6,15 @@ import com.mmall.dao.SysAclMapper;
 import com.mmall.dao.SysRoleAclMapper;
 import com.mmall.dao.SysRoleUserMapper;
 import com.mmall.model.SysAcl;
+import com.mmall.model.SysUser;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 @Service
 public class SysCoreService {
@@ -53,6 +57,42 @@ public class SysCoreService {
         return sysAclMapper.getByIdList(userAclIdList);
     }
     public boolean isSuperAdmin(){
-        return true;
+        //自己定义的规则,可以是配置文件获取
+        SysUser sysUser = RequestHolder.getCurrentUser();
+        if (sysUser.getMail().contains("admin")){
+            return true;
+        }
+        return false;
     }
+
+    public boolean hasUrlAcl(String url){
+        if (isSuperAdmin()){
+            return true;
+        }
+        //权限点为空的时候,直接true,说明不关心这个url
+        List<SysAcl> aclList = sysAclMapper.getByUrl(url);
+        if (CollectionUtils.isEmpty(aclList)){
+            return true;
+        }
+
+        List<SysAcl> userAclList = getCurrentUserAclList();
+        Set<Integer> userAclIdSet = userAclList.stream().map(SysAcl::getId).collect(toSet());
+        boolean hasValidAcl = false;
+        // 规则: 只要有一个权限点有权限,那么我们就认为有访问权限
+        for (SysAcl acl : aclList) {
+            //判断一个用户是否具有某个权限点的访问权限
+            if (acl == null || acl.getStatus() != 1) { //权限点无效
+                continue;
+            }
+            hasValidAcl = true;
+            if (userAclIdSet.contains(acl.getId())) {
+                return true;
+            }
+        }
+            if (!hasValidAcl){
+                return true;
+            }
+            return false;
+        }
 }
+
